43cf69c4b81121b67b218b380a10b9960773ce1e
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-10-04 04:17:13 +0300

    Edited product card for right content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,23 @@ export default {
   content: ['./**/*.{js,json,liquid}'],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 };
